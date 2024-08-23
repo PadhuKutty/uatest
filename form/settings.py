f3f2fc83284b1ad53bf7b14adb7213eb44cd126b
Padma Priya K <143091427+PadhuKutty@users.noreply.github.com>
2024-08-23 16:48:45 +0530

    Update settings.py

--- form/settings.py
+++ form/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 8080))
+s.connect(("8.8.8.8", 80))
 
 ALLOWED_HOSTS = ['.vercel.app']
 s.close()
